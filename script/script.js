07a1ea9790ea9dbd687f4602b298ae4b35ab1cc5
Amol Gupta <gupta07amol@gmail.com>
2024-10-01 18:38:17 +0530

    port-folio changes

---
+++ script/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const revealElements = document.querySelectorAll(".reveal");
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }
+  );
+
+  revealElements.forEach((element) => observer.observe(element));
+
+  const welcomeText = document.querySelector(".welcome-text");
+  const typingText = document.querySelector(".typing-effect");
+  const selectOption = document.querySelector(".select-option");
+  const clickPopUp = document.querySelector(".click-pop-up");
+
+  setTimeout(() => {
+    clickPopUp.style.display="block";
+    clickPopUp.style.opacity='1';
+    clickPopUp.style.transform= "translateY(0)";
+  }, 5000); // 5000 milliseconds = 5 seconds
+  
+  typingText && typingText.addEventListener("animationend", () => {
+    document.addEventListener("click", () => {
+      welcomeText.classList.add("hidden");
+      welcomeText.classList.add("transition-effect");
+      clickPopUp.classList.add("hidden");
+      setTimeout(() => {
+        selectOption.style.display = "flex";
+      }, 3000);
+    });
+  });
+});
